8fab712552253d005b03844bf3788aa2c1025a5a
Thomas Newlands <thomasnewlands@glowbox.io>
2020-08-24 11:47:03 -0700

    simple scene selection

---
+++ Assets/SceneSelect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelect : MonoBehaviour
+{
+    public string[] sceneNames;
+    int sceneSelection = -1;
+
+    void OnGUI(){
+        GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
+
+        sceneSelection = GUILayout.SelectionGrid(sceneSelection, sceneNames, 1);
+        if(sceneSelection != -1){
+            SceneManager.LoadScene(sceneNames[sceneSelection]);
+        }
+
+        GUILayout.EndArea ();
+
+    }
+}
